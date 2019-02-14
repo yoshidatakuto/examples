49f6af8c287f21e18f3672b573a3cd0991c690a2
yoshidatakuto <yoshidatakuto@yoshidatakutonoMacBook-puro.local>
2019-02-14 21:01:15 +0900

    fix

--- mnist/csv_manager.py
+++ mnist/csv_manager.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 def write_csv(csvfilename, avg_loss, epoch):
-    with open(csvfilename + '.csv', 'a') as f:
+    with open(csvfilename + '.csv', 'a',newline='') as f:
         writer = csv.writer(f, lineterminator='\n') # 改行コード（\n）を指定しておく
         writer.writerow(str(epoch))     # list（1次元配列）の場合
         writer.writerow(str(avg_loss))     # list（1次元配列）の場合
---
+++ mnist/view_loss.py
@@ -0,0 +1,2 @@
+from csv_manager import view_loss
+view_loss('mnist_loss')
